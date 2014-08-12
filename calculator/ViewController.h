76dc574438cca305e648e4034f4a37e133943f99
Dowlath <hibrise@IKRAMs-Mac-mini.local>
2014-08-12 13:08:31 +0530

    calc
    basic calc

--- calculator/ViewController.h
+++ calculator/ViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *display;
+- (IBAction)add:(id)sender;
+- (IBAction)sub:(id)sender;
+- (IBAction)multiply:(id)sender;
+- (IBAction)division:(id)sender;
+//@property (weak, nonatomic) IBOutlet UIButton *equal;
+
+-(void) process;
+@property (weak, nonatomic) IBOutlet UITextField *value1;
+@property (weak, nonatomic) IBOutlet UITextField *value2;
 
 @end
--- calculator/ViewController.m
+++ calculator/ViewController.m
@@ -9,11 +9,14 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+{
+    int set;
+}
 
 @end
 
 @implementation ViewController
-
+@synthesize value1,value2;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
@@ -24,6 +27,63 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)add:(id)sender {
+    set=1;
+    self.process;
+    
+   
+}
+
+- (IBAction)sub:(id)sender {
+    set=2;
+    self.process;}
+
+- (IBAction)multiply:(id)sender {
+    
+ set=3;
+self.process;
+}
+- (IBAction)division:(id)sender {
+    set=4;
+    self.process;}
+
+-(void) process
+{
+    int result,input1,input2;
+    input1=[[value1 text]intValue];
+    input2=[[value2 text ]intValue];
+    switch (set) {
+        case 1:
+            
+            result=input1+input2;
+    
+            [_display setText:[NSString stringWithFormat:@"%d",result]];
+       
+            break;
+       case 2:
+           result=input1-input2;
+          
+          
+            [_display setText:[NSString stringWithFormat:@"%d",result]];
+            break;
+        case 3:
+           result=input1*input2;
+         
+          
+            [_display setText:[NSString stringWithFormat:@"%d",result]];
+            break;
+        case 4:
+           result=input1/input2;
+         
+         
+            [_display setText:[NSString stringWithFormat:@"%d",result]];
+            break;
+        default:
+            NSLog(@"invalid selection");
+            break;
+    }
 }
 
 @end
